561fe94b49e95449d0d70075f2214f1b5e0b29ef
Kweenshaly7 <kweenzy80@yahoo.com>
2022-12-16 05:30:36 -0800

    print each character in putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - print _putchar
- * code by Kweenshaly7
- * Return: 0
+ * main - print each character in _putchar
+ *
+ * Return: always (0)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
