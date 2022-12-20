4b816115bb9798207a5f621d92603347751cdd3d
Kweenshaly7 <kweenzy80@yahoo.com>
2022-12-19 17:44:29 -0800

    Deferencing to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the value of a var using pointers
+ * main - update the value of a var using pointers
  * @n: int to update
  * Return: Always 0
  */
 
-void reset_to_98(int *n);
+int main(void)
 {
-	*n = 98;
+	int n;
+
+	int *p;
+
+	n = 98;
+	p = &n;
+	printf("Value of 'n': %d\n", n);
+	printf("Address of 'n': %p\n", &n);
+	printf("Value of 'p': %p\n", p);
+
+	*p = 402;
+	printf("Value of 'n': %d\n", n);
+
+	return (0);
 }
