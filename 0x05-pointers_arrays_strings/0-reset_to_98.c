cc399c359486d84f475d25a23d2569d65de80a4d
Kweenshaly7 <kweenzy80@yahoo.com>
2022-12-19 19:38:39 -0800

    Dereferncing to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,10 @@
-#include "0-main.h"
-#include <stdio.h>
-
+#include "main.h"
 /**
- * reset_to_98 - update the value of a var using pointers
- * @n: int to update
+ * reset_to_98 - Resets the value of a var to 98
+ * @n: var to be reset
  * Return: Always 0
  */
-
 void reset_to_98(int *n);
 {
 	*n = 98;
-
-	return (0);
 }
